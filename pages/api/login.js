adf83b2c80ab65df9314532aa367d526bc1b0014
devPremK <kumxrcodes@gmail.com>
2023-12-30 20:41:02 +0530

    s

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,8 +1,14 @@
-
+import Head from 'next/head'
 
 export default function Login(){
     return(
         <div>
+
+        <Head>
+            <title>Login</title>
+        </Head>
+
+
             <h1>Login</h1>
         </div>
     )
--- pages/api/register.js
+++ pages/api/register.js
@@ -1,8 +1,14 @@
-
+import Head from 'next/head'
 
 export default function Register(){
     return(
         <div>
+
+        <Head>
+            <title>Register</title>
+        </Head>
+
+
             <h1>Register</h1>
         </div>
     )
