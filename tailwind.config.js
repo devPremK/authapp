b76fc99f8d342f3b62bab38170c22ddd5c9eaec0
Zardex0008 <rudevera0008@gmail.com>
2023-12-31 12:02:19 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app//*.{js,ts,jsx,tsx,mdx}",
-    "./pages//.{js,ts,jsx,tsx,mdx}",
-    "./layout/**/.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
